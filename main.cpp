e488853e99cce3da7ee5008df8b551eaffe10f3f
Tiollit <Tiollit7@gmail.com>
2022-09-24 22:36:09 +0200

    Lab1 done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int main()
 {
-
-
+	cout << "\tName:        \"The war and the peace\"" << endl;
+	cout << "\tAvtor:       L.N. Tolstoy" << endl;
+	cout << "\tIzdatelstvo: Piter" << endl;
+	cout << "\tPages:       500." << endl;
+		 
 	return 0;
 }
