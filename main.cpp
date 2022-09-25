d010a7c7b1aa96c1db316497db5c4a79e2bcbea2
Tiollit <Tiollit7@gmail.com>
2022-09-25 12:52:51 +0200

    Lab 2 Done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int main()
 {
-
+	cout <<"\a\a\a\t" << char(34) << "Kray Lukomor'a dub zelenyj," << endl;
+	cout << "\a\a\a\tI zolotyj Lancyuh na nim:" << endl;
+	cout << "\a\a\a\tShodnya, shonochi kit uchenij" << endl;
+	cout << "\a\a\a\t\Na Lancyuhu kruzhl'aje tim\"" << endl;
 
 	return 0;
 }
