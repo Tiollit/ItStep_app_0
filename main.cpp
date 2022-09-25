41c9cfa078b3af5ee43e05ebf449ca5c8fc83a48
Tiollit <Tiollit7@gmail.com>
2022-09-25 13:17:08 +0200

    Lab 3 Done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 int main()
 {
-
-
+	cout << "\n\t" << char(201) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(187);
+	cout << "\n\t" << "";
+	cout << "\n\t" << char(186) << "          Pory Roku         " << char(186);
+	cout << "\n\t" << "";
+	cout << "\n\t" << char(204) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(203) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(203) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(203) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(185);
+	cout << "\n\t" << "";
+	cout << "\n\t" << char(186) << " Zyma " << char(186) << " Vesna " << char(186) << " Lito " << char(186) << " Osin " << char(186);
+	cout << "\n\t" << "";
+	cout << "\n\t" << char(200) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(188);
+	
 	return 0;
 }
