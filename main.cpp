1daca9df3613f97cd95c24e8d292a9173aa25ed5
Tiollit <Tiollit7@gmail.com>
2023-03-11 09:20:53 -0800

    g

--- main.cpp
+++ main.cpp
@@ -6,7 +6,35 @@ using namespace std;
 
 int main()
 {
+	bool Tr1 = false, Tr2 = false, Tr3 = false, Tr4 = false;
+	string A, B, C, D;
+	system("chcp 1251");
+	cout << "Проверка блока питания:" << endl;
+	cout << "На вопросы прошу отвечать да или нет" << endl;
+	while (Tr1 == false)
+	{
+		cout << "БП не видает напряжения?" << endl;
+		cin >> A;
+		if (A == "да")
+		{
+			cout << "Выполнить подключение БП к сети" << endl;
+		}
+		else
+		{
+			Tr1 == true;
+			Tr2 == true;
+		}
+		while (Tr2 == false)
+		{
+			cout << "Сетевое напряжение в наличии?" << endl;
+			cin >> B;
+			if (B == "да")
+			{
 
+			}
+		}
+	}
+	
 
 	return 0;
 }
